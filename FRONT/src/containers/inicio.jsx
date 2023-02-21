212dfaa50d9fad20f93b1e663ccbc49e3a0db637
Valeria Molina <102936189+Valeabril11@users.noreply.github.com>
2023-02-21 17:06:23 -0300

    se inician las rutas

---
+++ FRONT/src/containers/inicio.jsx
@@ -0,0 +1,309 @@
+//rsc
+
+import React from "react";
+import '../styles/Styles.scss';
+
+const Inicio = () => {
+  return (
+    <section className="main-container">
+      <div className="cards-container">
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="product-container">
+          <img src="./products/bob.jpeg" alt />
+          <div className="product-info">
+            <div>
+              <p>$2000,00</p>
+              <p>Tazon Bob Esponja</p>
+            </div>
+            <figure>
+              <img src="./icons/bt_add_to_cart.svg" alt />
+            </figure>
+          </div>
+          <form className="form" action="./5-screen desktop.html">
+            <div>
+              <input
+                type="submit"
+                defaultValue="Comprar"
+                className="primary-button"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Inicio;
