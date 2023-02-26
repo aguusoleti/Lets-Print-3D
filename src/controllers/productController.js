40736d8458888c1430a4b94a8144e51e8a1c2e1e
aguusoleti <agustinsoleti95@gmail.com>
2023-02-26 14:13:39 -0300

    Carga de fotos al modelo

---
+++ src/controllers/productController.js
@@ -0,0 +1,83 @@
+import Category from "../models/category.js";
+import Product from "../models/product.js";
+import subCategory from "../models/subCategory.js";
+import multer from 'multer';
+
+const createProducts = async (req, res) => {
+  try {
+    const subCategoriaTaza = await subCategory.findOne({ nombre: 'Tazones' });
+    const categoriaCeramica = await Category.findOne({ nombre: 'ceramica' });
+    const product = new Product(req.body,{subCategoriaTaza});
+    const productSave = await product.save();
+    res.json(productSave);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const searchProducts = async(req,res)=>{
+  const {nombre} = req.body;
+  try {
+    const data = await Product.find({ nombre: { $regex: nombre, $options: "i" } });
+    console.log(data);
+    res.status(200).json(data);
+  } catch (err) {
+    const error = new Error("Error al buscar un servicio");
+    res.status(404).json({ msg: error.message });
+  }
+};
+const searchAllProducts = async(req,res)=>{
+  try {
+    const data = await Product.find({});
+    console.log(data);
+    res.status(200).json(data);
+  } catch (err) {
+    const error = new Error("Error al buscar un servicio");
+    res.status(404).json({ msg: error.message });
+  }
+};
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+const uploadImage = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+
+    upload.array('images')(req, res, async function (err) {
+      if (err) {
+        return res.status(400).json({ message: 'Error al subir las imágenes' });
+      }
+
+      const images = req.files.map(file => {
+        return  file.path
+        
+      });
+console.log(images)
+      product.images = images;
+    
+      await product.save();
+
+      return res.status(200).json({ message: 'Imágenes subidas correctamente' });
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
+
+export {
+  createProducts,
+  searchProducts,
+  searchAllProducts,
+  uploadImage
+}
