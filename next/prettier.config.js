b14b3805b954853e833f89b661ae517a7741f41a
Valeria Molina <102936189+Valeabril11@users.noreply.github.com>
2023-03-04 13:17:11 -0300

    next

--- next/prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-    semi: true, // ;
-    singleQuote: true, // uso de la comilla simple 
-    printWidth: 200, // limite de los caracteres que pueden aparecer en pantalla
-    tabWidth: 2,
-    useTabs: false,
-    trailingComma: 'es6',
-    bracketSpacing: true,
-}
