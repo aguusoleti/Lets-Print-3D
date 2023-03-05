3341e47c07c2e979fa83c039bf63e7a82a7d6fbf
Valeria Molina <102936189+Valeabril11@users.noreply.github.com>
2023-03-05 20:47:39 -0300

    otra

---
+++ Frontend/src/pages/Madera/index.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Footer from '../../components/Footer/Footer.js';
+import AppBar from "../../components/Navbar/AppBar.js";
+
+function HomePage() {
+    return (
+    <div>
+
+     <AppBar/> 
+
+     <Footer/>
+
+   
+    </div>
+     )
+
+
+  }
+  
+  export default HomePage
--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -1,15 +1,18 @@
 import * as React from 'react';
+import Footer from '../components/Footer/Footer.js';
 import AppBar from "../components/Navbar/AppBar.js";
 import Banner from '../components/Banner/Banner.js'
-
 
 function HomePage() {
     return (
     <div>
      <AppBar/>
-
+     <Banner/>
+     <Footer/>
     </div>
-      )
+     )
+
+
   }
   
   export default HomePage
