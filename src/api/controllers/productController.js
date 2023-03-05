412e5aff02d0b41a9bf9fefacde4a58d483a3e49
josebrzmodo <jose.brizuela@modo.com.ar>
2023-03-05 15:41:46 -0300

    mf

--- src/api/controllers/productController.js
+++ src/api/controllers/productController.js
@@ -17,9 +17,8 @@ const createProducts = async (req, res) => {
 const searchProducts = async (req, res) => {
   const{name}=req.query
   try {
-    const data = await findAllAndPopulate(buildFilterProductQuery(req.query));  
+    const data = await findAllAndPopulate(buildFilterProductQuery(req.query));
     res.status(200).json(data);
-    
   } catch (err) {
     console.log(err.message)
     res.status(500).json({message: "internal server error"});
--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
-import express from "express";
-import conectarDB from "./database/db.js";
 import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+import express from "express";
 import cors from "cors";
+import conectarDB from "./database/db.js";
 
 import clientRoutes from "./api/routes/clientRoutes.js";
 import productRoutes from "./api/routes/productRoutes.js";
 import categoryRoutes from "./api/routes/categoryRoutes.js";
-
-dotenv.config({ path: ".env" });
-
-const app = express();
-conectarDB();
-app.use(cors());
-app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("¡Hola, mundo!");
-})
-
-app.use("/api/client", clientRoutes);
-app.use("/api/product", productRoutes);
-app.use("/api/category", categoryRoutes);
-
-
-
-
-
-// Arrancar el servidor
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor arrancado en el puerto ${PORT}`);
-});
+
+(async () => {
+  await conectarDB();
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.get("/", (req, res) => {
+    res.send("¡Hola, mundo!");
+  })
+  app.use("/api/client", clientRoutes);
+  app.use("/api/product", productRoutes);
+  app.use("/api/category", categoryRoutes);
+
+  app.listen(PORT, () => {
+    console.log(`Servidor arrancado en el http://localhost:${PORT}`);
+  });
+})();
