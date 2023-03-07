138cd81972d74e53db9a23eca79cd0265b7b504e
aguusoleti <agustin_soleti@hotmail.com>
2023-03-06 22:19:44 -0300

    Alguans modificaciones en Navbar

--- Frontend/src/pages/login.js
+++ Frontend/src/pages/login.js
@@ -4,24 +4,14 @@ import AppBar from "../components/Navbar/AppBar.js";
 import Banner from '../components/Banner/Banner.js';
 import Home from '../containers/Home/Home.js';
 
-function HomePage() {
+function Login() {
     return (
     <div>
-
-
-
-
      <AppBar/> 
-
      <Banner/>
      <Home/>
      <Footer/>
-
-   
     </div>
-     )
-
-
-  }
+     )  }
   
-  export default HomePage
+  export default Login
