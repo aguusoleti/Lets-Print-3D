cbccf93aba8551c36ad1fdf696688c07f2d92ee2
aguusoleti <agustinsoleti95@gmail.com>
2024-09-22 18:35:54 -0300

    Correcciones

--- src/database/db.js
+++ src/database/db.js
@@ -1,20 +1,23 @@
 import mongoose from "mongoose";
 
 
-export default (async () => {
+
+
+
+
+
+const conectarDB = async () => {
   const URI_DB="mongodb+srv://letsprint:QpK4KXd6w4xP7cgN@lets-print.tdue4ze.mongodb.net/?retryWrites=true&w=majority&appName=Lets-Print"
   try {
-    // const db = await mongoose.connect(process.env.MONGO_URI, {
-      const db = await mongoose.connect(URI_DB, {
-
+    const db = await mongoose.connect(URI_DB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(
-      `MongoDB conectado en ${db.connection.host}:${db.connection.port}`
-    );
+    console.log(`MongoDB conectado en ${db.connection.host}:${db.connection.port}`);
   } catch (error) {
     console.error(`Error al conectar con MongoDB: ${error}`);
     process.exit(1);
   }
 };
+
+export default conectarDB;
